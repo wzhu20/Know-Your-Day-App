84f4e28ccdfc563d82d99c138768166bf4b2fc10
wilsonzhu <wzhu20>
2018-03-03 21:11:13 -0500

    started calendar app only has base activity

---
+++ app/src/main/java/com/example/wilsonzhu/calendar_app/activity/CalendarActivity.java
@@ -0,0 +1,15 @@
+package com.example.wilsonzhu.calendar_app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wilsonzhu.calendar_app.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
