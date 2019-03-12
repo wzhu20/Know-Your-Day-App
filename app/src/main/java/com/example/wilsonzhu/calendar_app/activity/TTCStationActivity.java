6986d6a4c727c53a429788e3178ca348de923722
wilson.zhu <wilson.zhu20@gmail.com>
2019-03-11 22:00:42 -0400

    added empty recycler view template

---
+++ app/src/main/java/com/example/wilsonzhu/calendar_app/activity/TTCStationActivity.java
@@ -0,0 +1,11 @@
+package com.example.wilsonzhu.calendar_app.activity;
+
+import android.os.Bundle;
+
+public class TTCStationActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+    }
+}
