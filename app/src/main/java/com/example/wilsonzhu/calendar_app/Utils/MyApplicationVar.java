97989e8ddf865b97da172d9d4a7784691d1780e6
wilson.zhu <wilson.zhu20@gmail.com>
2019-04-04 00:14:38 -0400

    using a different api now.. abandoned  the old one

--- app/src/main/java/com/example/wilsonzhu/calendar_app/Utils/MyApplicationVar.java
+++ app/src/main/java/com/example/wilsonzhu/calendar_app/Utils/MyApplicationVar.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 public class MyApplicationVar extends Application {
     private int LINECHOSEN = 0;
+    private String STATION = "finch_station";
 
     public boolean setLineChosen(int lineChosen) {
         if (lineChosen > 4 || lineChosen < 1) {
@@ -15,5 +16,13 @@ public class MyApplicationVar extends Application {
 
     public int getLineChosen() {
         return LINECHOSEN;
+    }
+
+    public void setStationChosen(String stationName) {
+        STATION = stationName;
+    }
+
+    public String getStation() {
+        return STATION;
     }
 }
---
+++ app/src/main/java/com/example/wilsonzhu/calendar_app/activity/BusesActivity.java
@@ -0,0 +1,69 @@
+package com.example.wilsonzhu.calendar_app.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.VolleyError;
+import com.example.wilsonzhu.calendar_app.Adapter.BusesAdapter;
+import com.example.wilsonzhu.calendar_app.HTTPClient.HTTPErrorListener;
+import com.example.wilsonzhu.calendar_app.HTTPClient.HTTPRequestHandler;
+import com.example.wilsonzhu.calendar_app.HTTPClient.HTTPRequestListener;
+import com.example.wilsonzhu.calendar_app.R;
+import com.example.wilsonzhu.calendar_app.Utils.MyApplicationVar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class BusesActivity extends BaseActivity {
+    private final String URL = "http://webservices.nextbus.com/service/publicJSONFeed?command=routeList&a=ttc";
+
+    private ArrayList<String> allBuses = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private HTTPRequestHandler httpRequestHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ttc_station);
+        httpRequestHandler = new HTTPRequestHandler();
+        initData();
+        recyclerView = findViewById(R.id.ttc_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void initData() {
+        // Make Api call to Myttc to get all buses to a station and display it
+        String path = ((MyApplicationVar) this.getApplication()).getStation();
+        httpRequestHandler.HTTPGetRequest(URL , new HTTPRequestListener() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray stops = jsonObject.getJSONArray("route");
+                    if (stops != null) {
+                        for(int i = 0; i < stops.length(); i++) {
+                            allBuses.add(((JSONObject)stops.get(i)).getString("title"));
+                        }
+                        mAdapter = new BusesAdapter(allBuses);
+                        recyclerView.setAdapter(mAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new HTTPErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }, "made request to get this station: " + path, this);
+    }
+}
